1fed312590add3458cbae5bb072f6ff5ce66e751
lotfi37 <lotfi.benabdellaziz@fst.utm.tn>
2025-06-25 08:46:35 +0200

    Remove data from tracking

--- proejct22/inference/batch_predict.py
+++
@@ -1,6 +0,0 @@
-import mlflow.sklearn
-import pandas as pd
-
-def load_model_and_predict(input_df):
-    model = mlflow.sklearn.load_model('runs:/your_run_id/model')
-    return model.predict(input_df)
--- proejct22/inference/spark_predict.py
+++
@@ -1,7 +0,0 @@
-import mlflow
-from pyspark.sql.functions import struct, col
-
-def run_spark_prediction(spark, df):
-    logged_model = 'runs:/your_run_id/model'
-    loaded_model = mlflow.pyfunc.spark_udf(spark, model_uri=logged_model)
-    return df.withColumn('predictions', loaded_model(struct(*map(col, df.columns))))
