47628647e0eafa41f191dc2d1a3d226a722c5372
lotfi37 <lotfi.benabdellaziz@fst.utm.tn>
2025-06-25 08:50:48 +0200

    Remove Unnecessary folder  from tracking

--- Project/inference/spark_predict.py
+++
@@ -1,9 +0,0 @@
-import mlflow
-from pyspark.sql.functions import struct, col
-logged_model = 'runs:/6fad7ce1c0134d0b8ab8dca931773bd2/model'
-
-# Load model as a Spark UDF.
-loaded_model = mlflow.pyfunc.spark_udf(spark, model_uri=logged_model)
-
-# Predict on a Spark DataFrame.
-df.withColumn('predictions', loaded_model(struct(*map(col, df.columns))))
